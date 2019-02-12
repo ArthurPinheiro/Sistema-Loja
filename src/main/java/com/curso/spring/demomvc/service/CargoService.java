82f65884b06693d3765d07b9dd5e32119b0c04ca
ArthurPinheiro <arthurpinheirodelima@gmail.com>
2019-02-11 22:53:22 -0300

    Adicionando classes de serviço e mudança na arquitetura.

---
+++ src/main/java/com/curso/spring/demomvc/service/CargoService.java
@@ -0,0 +1,14 @@
+package com.curso.spring.demomvc.service;
+
+import com.curso.spring.demomvc.domain.Cargo;
+
+import java.util.List;
+
+public interface CargoService {
+
+    void salvar(Cargo cargo);
+    void editar(Cargo cargo);
+    void excluir(Long id);
+    Cargo buscarPorId(Long id);
+    List<Cargo> buscarTodos();
+}
---
+++ src/main/java/com/curso/spring/demomvc/service/DepartamentoServiceImpl.java
@@ -0,0 +1,44 @@
+package com.curso.spring.demomvc.service;
+
+import com.curso.spring.demomvc.dao.DepartamentoDao;
+import com.curso.spring.demomvc.domain.Departamento;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DepartamentoServiceImpl implements DepartamentoService {
+
+    @Autowired
+    private DepartamentoDao dao;
+
+    @Transactional
+    @Override
+    public void salvar(Departamento departamento) {
+        dao.save(departamento);
+    }
+
+    @Transactional
+    @Override
+    public void editar(Departamento departamento) {
+        dao.update(departamento);
+    }
+
+    @Transactional
+    @Override
+    public void excluir(Long id) {
+        dao.delete(id);
+    }
+
+    @Override
+    public Departamento buscarPorId(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Departamento> buscarTodos() {
+        return dao.findAll();
+    }
+}
