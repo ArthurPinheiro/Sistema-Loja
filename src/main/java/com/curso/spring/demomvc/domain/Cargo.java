a06ce6cd9cc67c4ba65b70a991a0c0505cc0cd85
ArthurPinheiro <arthurpinheirodelima@gmail.com>
2019-02-17 23:24:48 -0300

    Ajuste e criação de novas views.

--- src/main/java/com/curso/spring/demomvc/domain/Cargo.java
+++ src/main/java/com/curso/spring/demomvc/domain/Cargo.java
@@ -13,7 +13,7 @@ public class Cargo extends AbstractEntity<Long> {
     @Column(name = "nome", nullable = false, unique = true, length = 60)
     private String nome;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "id_departamento_fk")
     private Departamento departamento;
 
